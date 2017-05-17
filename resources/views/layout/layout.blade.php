bf76bc62c27802c3cf1d97bd99e0de90edfe5da1
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-05-17 14:07:44 +0530

    minor changes

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -2,10 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8"/>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     {{--<meta http-equiv="Refresh" content="number">--}}
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
 </head>
 <body>
     @yield('body_content')
+    {{Html::script('js/app.js')}}
 </body>
 </html>
