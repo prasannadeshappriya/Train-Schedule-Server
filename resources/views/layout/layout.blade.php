9d2d70ba23850ab3638a0afa9002641ec6851580
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-05-17 14:47:35 +0530

    minor changes

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{--<meta http-equiv="Refresh" content="number">--}}
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="shortcut icon" href="{{asset('img/icon.png')}}">
 </head>
 <body style="margin-bottom: 60px">
     @yield('body_content')
